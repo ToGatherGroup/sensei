d9d6df021f8f02e77d353037a5d8d5bf57e16090
Michel Machado <michelmachado125@gmail.com>
2024-02-21 23:37:40 -0300

    cornojob 2

--- src/main/java/com/togather/sensei/SenseiApplication.java
+++ src/main/java/com/togather/sensei/SenseiApplication.java
@@ -1,9 +1,20 @@
 package com.togather.sensei;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Projeto Sensei",
+				version = "1.0",
+				description = "Projeto de software para avaliação e controle físicos de alunos de judô do Projeto Sensei Divino",
+				contact = @Contact(name = "Equipe ToGather")
+		)
+)
 public class SenseiApplication {
 	public String PORT = System.getenv("PORT");
 
