3f376cdb741a284457b3b077b8de9b49bf40a426
Michel Machado <michelmachado125@gmail.com>
2024-03-22 21:18:36 -0300

    busca por nome paginada

--- src/main/java/com/togather/sensei/repositories/AtletaRepository.java
+++ src/main/java/com/togather/sensei/repositories/AtletaRepository.java
@@ -1,10 +1,18 @@
 package com.togather.sensei.repositories;
 
 import com.togather.sensei.models.AtletaModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AtletaRepository extends JpaRepository<AtletaModel, Long> {
     AtletaModel findByNome(String nome);
+
+    @Query(nativeQuery = true, value = "SELECT * from atleta_tb where nome like  :nome% ;")
+    Page<AtletaModel> buscaPorNome(String nome, Pageable pageable);
 }
