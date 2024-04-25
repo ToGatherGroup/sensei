d7dec08845820d23aa83f72b55efb804523499d2
Denilson Marcio <2212811@aluno.univesp.com>
2024-04-24 23:37:04 -0300

    [PISD-167] Busca avaliações incompletas para cada atleta.

---
+++ src/main/java/com/togather/sensei/services/avaliacaoService/AvaliacaoService.java
@@ -0,0 +1,9 @@
+package com.togather.sensei.services.avaliacaoService;
+
+import com.togather.sensei.DTO.avaliacao.AvaliacaoIncompletaDTO;
+
+import java.util.List;
+
+public interface AvaliacaoService {
+    List<AvaliacaoIncompletaDTO> buscaAvaliacoesIncompletas();
+}
