aa1dd1b560e1aaa63091116f9c5ad775c4a09b8b
Karen Boscolo <karenboscolo@yahoo.com.br>
2024-05-09 21:19:49 -0300

    Chave composta avaliação Model, retornando Void.

---
+++ src/main/java/com/togather/sensei/models/AvaliacaoModelId.java
@@ -0,0 +1,20 @@
+package com.togather.sensei.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class AvaliacaoModelId {
+    @ManyToOne
+    @JoinColumn(name = "atleta_id")
+    private AtletaModel atletaModel;
+    @Temporal(TemporalType.DATE)
+    private LocalDate data;
+}
