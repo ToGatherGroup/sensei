486e88666470fe0ffb5d80f703eded3f6f27bb6d
Denilson Marcio <2212811@aluno.univesp.com>
2024-05-14 22:53:06 -0300

    [BUGFIX] ajuste-endpoint-busca-avaliaçao-incompleta.

--- src/main/java/com/togather/sensei/repositories/AvaliacaoRepository.java
+++ src/main/java/com/togather/sensei/repositories/AvaliacaoRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 @Repository
 public interface AvaliacaoRepository extends JpaRepository <AvaliacaoModel, Long> {
     @Query(value = """
-            SELECT data, abdominais, altura, burpees, cooper, flexoes, forca_isometrica_maos, peso, prancha, rm_terra, teste_de_lunge, atleta_id
+            SELECT data, abdominais, altura, burpees, cooper, flexoes, forca_isometrica_maos, peso, prancha, rm_terra, teste_de_lunge, impulsao_vertical, atleta_id
             FROM (
                 SELECT\s
-                    data, abdominais, altura, burpees, cooper, flexoes, forca_isometrica_maos, peso, prancha, rm_terra, teste_de_lunge, atleta_id,
+                    data, abdominais, altura, burpees, cooper, flexoes, forca_isometrica_maos, peso, prancha, rm_terra, teste_de_lunge, impulsao_vertical, atleta_id,
                     ROW_NUMBER() OVER (PARTITION BY atleta_id ORDER BY data DESC) AS row_num
                 FROM avaliacao_tb
                 WHERE\s
@@ -26,7 +26,8 @@ public interface AvaliacaoRepository extends JpaRepository <AvaliacaoModel, Long
                     peso IS NULL OR
                     prancha IS NULL OR
                     rm_terra IS NULL OR
-                    teste_de_lunge IS NULL
+                    teste_de_lunge IS NULL OR
+                    impulsao_vertical IS NULL
             ) AS sub
             WHERE row_num = 1""", nativeQuery = true)
     List<AvaliacaoModel> getAvaliacoesIncompletas();
