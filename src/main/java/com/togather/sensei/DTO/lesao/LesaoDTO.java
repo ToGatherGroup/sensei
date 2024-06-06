5058008076d2633c279c0bb389e252bf3775598f
togather <togatherthegroup@gmail.com>
2024-06-06 14:57:56 -0300

    [PISD-241]] criar-DTO-de-lesoes-removendo-dados-do-atleta

---
+++ src/main/java/com/togather/sensei/DTO/lesao/LesaoDTO.java
@@ -0,0 +1,21 @@
+package com.togather.sensei.DTO.lesao;
+
+import com.togather.sensei.enums.RegiaoCorpoEnum;
+import com.togather.sensei.models.AtletaModel;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class LesaoDTO {
+
+
+    private LocalDate data;
+    private String descricao;
+    private RegiaoCorpoEnum regiaoLesao;
+
+    private Long atletaId;
+}
