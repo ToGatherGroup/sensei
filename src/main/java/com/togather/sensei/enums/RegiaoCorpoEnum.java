9dd4580a649e04cceef8a7da20647ffb43de67d5
Michel <michelmachado125@gmail.com>
2024-06-26 16:49:31 -0300

    ajuste enums boneco

--- src/main/java/com/togather/sensei/enums/RegiaoCorpoEnum.java
+++ src/main/java/com/togather/sensei/enums/RegiaoCorpoEnum.java
@@ -1,7 +1,7 @@
 package com.togather.sensei.enums;
 
 public enum RegiaoCorpoEnum {
-    ANTERIOR_CABECA("Cabeça Anterior"),
+    CABECA("Cabeça Anterior"),
     ANTERIOR_TORAXICA("Anterior Toráxica"),
     ANTERIOR_ABDOMINAL("Anterior Abdominal"),
     ANTERIOR_PELVICA("Anterior Pélvica"),
@@ -9,7 +9,7 @@ public enum RegiaoCorpoEnum {
     ANTERIOR_OMBRO_DIREITO("Anterior Ombro Direito"),
     ANTERIOR_BRACO_DIREITO("Anterior Braço Direito"),
     ANTERIOR_ANTEBRACO_DIREITO("Anterior Antebraço Direito"),
-    ANTERIOR_MAO_DIREITO("Anterior Mão Direita"),
+    MAO_DIREITA("Anterior Mão Direita"),
     ANTERIOR_FERMURAL_DIREITO("Anterior Femural Direito"),
     ANTERIOR_JOELHO_DIREITO("Anterior Joelho Direito"),
     ANTERIOR_CRURAL_DIREITO("Anterior Crural Direito"),
@@ -18,7 +18,7 @@ public enum RegiaoCorpoEnum {
     ANTERIOR_OMBRO_ESQUERDO("Anterior Ombro Esquerdo"),
     ANTERIOR_BRACO_ESQUERDO("Anterior Braço Esquerdo"),
     ANTERIOR_ANTEBRACO_ESQUERDO("Anterior Antebraço Esquerdo"),
-    ANTERIOR_MAO_ESQUERDA("Anterior Mão Esquerda"),
+    MAO_ESQUERDA("Anterior Mão Esquerda"),
     ANTERIOR_FERMURAL_ESQUERDO("Anterior Femural Esquerdo"),
     ANTERIOR_JOELHO_ESQUERDO("Anterior Joelho Esquerdo"),
     ANTERIOR_CRURAL_ESQUERDO("Anterior Crural Esquerdo"),
