412a55cfc0d8c9576e63b17492e89f3d660faf95
Denilson Marcio <denilsonmarcio@hotmail.com>
2024-07-18 01:31:08 -0300

    [PISD-311] - editar tabela de avaliações

--- src/main/java/com/togather/sensei/services/avaliacaoService/impl/AvaliacoesPorAtletaServiceImpl.java
+++ src/main/java/com/togather/sensei/services/avaliacaoService/impl/AvaliacoesPorAtletaServiceImpl.java
@@ -3,7 +3,6 @@ package com.togather.sensei.services.avaliacaoService.impl;
 import com.togather.sensei.DTO.geral.MetricaAvaliacao;
 import com.togather.sensei.DTO.geral.SeriesDTO;
 import com.togather.sensei.enums.AvaliacaoEnum;
-import com.togather.sensei.exceptions.BusinessException;
 import com.togather.sensei.exceptions.NotFoundException;
 import com.togather.sensei.models.AtletaModel;
 import com.togather.sensei.models.AvaliacaoModel;
@@ -21,15 +20,17 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class AvaliacoesPorAtletaServiceImpl implements AvaliacoesPorAtletaService {
+public class AvaliacoesPorAtletaServiceImpl implements AvaliacoesPorAtletaService
+{
     private final AvaliacaoRepository avaliacaoRepository;
     private final AtletaRepository atletaRepository;
 
-    public List<MetricaAvaliacao> getMetricasAvaliacao() {
+    public List<MetricaAvaliacao> getMetricasAvaliacao()
+    {
         List<MetricaAvaliacao> lstMetricas = new ArrayList<>();
 
         MetricaAvaliacao metricaTemp = new MetricaAvaliacao();
-        metricaTemp.setTipoAvaliacao(AvaliacaoEnum.Core);
+        metricaTemp.setTipoAvaliacao( AvaliacaoEnum.Core );
         metricaTemp.setDescricao("Core");
         metricaTemp.setMinino(0.);
         metricaTemp.setMaximo(8.);
@@ -37,7 +38,7 @@ public class AvaliacoesPorAtletaServiceImpl implements AvaliacoesPorAtletaServic
         lstMetricas.add(metricaTemp);
 
         metricaTemp = new MetricaAvaliacao();
-        metricaTemp.setTipoAvaliacao(AvaliacaoEnum.ForcaMaximna);
+        metricaTemp.setTipoAvaliacao( AvaliacaoEnum.ForcaMaximna );
         metricaTemp.setDescricao("Força Máxima");
         metricaTemp.setMinino(0.);
         metricaTemp.setMaximo(150.);
@@ -45,7 +46,7 @@ public class AvaliacoesPorAtletaServiceImpl implements AvaliacoesPorAtletaServic
         lstMetricas.add(metricaTemp);
 
         metricaTemp = new MetricaAvaliacao();
-        metricaTemp.setTipoAvaliacao(AvaliacaoEnum.ForcaExplosiva);
+        metricaTemp.setTipoAvaliacao( AvaliacaoEnum.ForcaExplosiva );
         metricaTemp.setDescricao("Força Explosiva");
         metricaTemp.setMinino(0.);
         metricaTemp.setMaximo(65.);
@@ -53,7 +54,7 @@ public class AvaliacoesPorAtletaServiceImpl implements AvaliacoesPorAtletaServic
         lstMetricas.add(metricaTemp);
 
         metricaTemp = new MetricaAvaliacao();
-        metricaTemp.setTipoAvaliacao(AvaliacaoEnum.ForcaIsometrica);
+        metricaTemp.setTipoAvaliacao( AvaliacaoEnum.ForcaIsometrica );
         metricaTemp.setDescricao("Força Isométrica");
         metricaTemp.setMinino(0.);
         metricaTemp.setMaximo(5.);
@@ -77,7 +78,7 @@ public class AvaliacoesPorAtletaServiceImpl implements AvaliacoesPorAtletaServic
         lstMetricas.add(metricaTemp);
 
         metricaTemp = new MetricaAvaliacao();
-        metricaTemp.setTipoAvaliacao(AvaliacaoEnum.ResistenciaMuscularLocalizadaAbdominal);
+        metricaTemp.setTipoAvaliacao( AvaliacaoEnum.ResistenciaMuscularLocalizadaAbdominal);
         metricaTemp.setDescricao("RML Abdominal");
         metricaTemp.setMinino(0.);
         metricaTemp.setMaximo(60.);
@@ -85,7 +86,7 @@ public class AvaliacoesPorAtletaServiceImpl implements AvaliacoesPorAtletaServic
         lstMetricas.add(metricaTemp);
 
         metricaTemp = new MetricaAvaliacao();
-        metricaTemp.setTipoAvaliacao(AvaliacaoEnum.ResistenciaMuscularLocalizadaMMSS);
+        metricaTemp.setTipoAvaliacao( AvaliacaoEnum.ResistenciaMuscularLocalizadaMMSS );
         metricaTemp.setDescricao("RML MMSS");
         metricaTemp.setMinino(0.);
         metricaTemp.setMaximo(50.);
@@ -93,7 +94,7 @@ public class AvaliacoesPorAtletaServiceImpl implements AvaliacoesPorAtletaServic
         lstMetricas.add(metricaTemp);
 
         metricaTemp = new MetricaAvaliacao();
-        metricaTemp.setTipoAvaliacao(AvaliacaoEnum.ResistenciaAnaerobica);
+        metricaTemp.setTipoAvaliacao( AvaliacaoEnum.ResistenciaAnaerobica );
         metricaTemp.setDescricao("R. Anaeróbica");
         metricaTemp.setMinino(0.);
         metricaTemp.setMaximo(40.);
@@ -101,7 +102,7 @@ public class AvaliacoesPorAtletaServiceImpl implements AvaliacoesPorAtletaServic
         lstMetricas.add(metricaTemp);
 
         metricaTemp = new MetricaAvaliacao();
-        metricaTemp.setTipoAvaliacao(AvaliacaoEnum.ResistenciaAerobica);
+        metricaTemp.setTipoAvaliacao( AvaliacaoEnum.ResistenciaAerobica );
         metricaTemp.setDescricao("R. Aeróbica");
         metricaTemp.setMinino(0.);
         metricaTemp.setMaximo(3000.);
@@ -111,112 +112,108 @@ public class AvaliacoesPorAtletaServiceImpl implements AvaliacoesPorAtletaServic
         return lstMetricas;
     }
 
-    private double getPercentual(double min, double max, double valor) {
-        if(valor > max)
-            valor = max;
-
+    private double getPercentual(double min, double max, double valor)
+    {
         double valorMaximo = max - min;
         return (valor / valorMaximo) * 100;
     }
 
-    private double getPercentualInvertido(double min, double max, double valor) {
-        if(valor > min)
-            valor = min;
-
-        if(valor < max)
-            valor = max;
-
+    private double getPercentualInvertido(double min, double max, double valor)
+    {
         double valorMinimo = min - max;
-        return (1 - (valor / valorMinimo)) * 100;
+        return (1 - (valor/valorMinimo)) * 100;
     }
 
     @Override
-    public SeriesDTO getAvaliacoesPorAtleta(Long atletaId) {
-        List<MetricaAvaliacao> lstMetricas = getMetricasAvaliacao();
+    public SeriesDTO getAvaliacoesPorAtleta(Long atletaId)
+    {
+        try
+        {
+            List<MetricaAvaliacao> lstMetricas = getMetricasAvaliacao();
 
-        SeriesDTO dto = new SeriesDTO();
-        List<String> labels = new ArrayList<>();
-        List<Double> values = new ArrayList<>();
+            SeriesDTO dto = new SeriesDTO();
+            List<String> labels = new ArrayList<>();
+            List<Double> values = new ArrayList<>();
 
-        Optional<AtletaModel> optionalAtletaModel = atletaRepository.findById(atletaId);
-        if (optionalAtletaModel.isEmpty())
-            throw new NotFoundException("Nenhum atleta encontrado com o id " + atletaId);
+            Optional<AtletaModel> optionalAtletaModel =atletaRepository.findById(atletaId);
+            if(optionalAtletaModel.isEmpty())
+                throw new NotFoundException("Nenhum atleta encontrado com o id " + atletaId);
 
-        AtletaModel atleta = optionalAtletaModel.get();
-        AvaliacaoModel avaliacao = avaliacaoRepository.getLastAvaliacaoByAtleta(atletaId);
-        if (avaliacao == null)
-            throw new NotFoundException("Nenhuma avaliação encontrada para o atleta " + atletaId);
+            AtletaModel atleta = optionalAtletaModel.get();
+            AvaliacaoModel avaliacao = avaliacaoRepository.getLastAvaliacaoByAtleta(atletaId);
+            if(avaliacao == null)
+                throw new NotFoundException("Nenhuma avaliação encontrada para o atleta " + atletaId);
 
-        LocalDate dataAtual = LocalDate.now();
+            LocalDate dataAtual = LocalDate.now();
 
-        metrica = lstMetricas.get( AvaliacaoEnum.MobilidadeDoTornozeloDireito.ordinal() );
-        labels.add(metrica.getDescricao());
-        valorTemporario = getPercentualInvertido(metrica.getMinino(), metrica.getMaximo(), avaliacao.getTesteDeLungeJoelhoDireito());
-        values.add(valorTemporario);
+            if (atleta.getNascimento() == null) {
+                throw new NotFoundException("Data de nascimento do atleta não especificada");
+            }
 
-        metrica = lstMetricas.get( AvaliacaoEnum.MobilidadeDoTornozeloEsquerdo.ordinal() );
-        labels.add(metrica.getDescricao());
-        valorTemporario = getPercentualInvertido(metrica.getMinino(), metrica.getMaximo(), avaliacao.getTesteDeLungeJoelhoEsquerdo());
-        values.add(valorTemporario);
+            Period periodoEntreAsDatas = Period.between(atleta.getNascimento(), dataAtual);
+            int idadeDoAtleta = periodoEntreAsDatas.getYears();
 
-        metrica = lstMetricas.get( AvaliacaoEnum.ResistenciaMuscularLocalizadaAbdominal.ordinal() );
-        labels.add(metrica.getDescricao());
-        valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getAbdominais());
-        values.add(valorTemporario);
-
-        Period periodoEntreAsDatas = Period.between(atleta.getNascimento(), dataAtual);
-        int idadeDoAtleta = periodoEntreAsDatas.getYears();
-
-        MetricaAvaliacao metrica = lstMetricas.get(AvaliacaoEnum.Core.ordinal());
-        labels.add(metrica.getDescricao());
-        double valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getPrancha().toMinutes());
-        values.add(valorTemporario);
-
-        if (idadeDoAtleta > 11) {
-            metrica = lstMetricas.get(AvaliacaoEnum.ForcaMaximna.ordinal());
+            MetricaAvaliacao metrica = lstMetricas.get( AvaliacaoEnum.Core.ordinal() );
             labels.add(metrica.getDescricao());
-            valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getRmTerra());
+            double valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getPrancha().toMinutes());
             values.add(valorTemporario);
+
+            if(idadeDoAtleta > 11)
+            {
+                metrica = lstMetricas.get(AvaliacaoEnum.ForcaMaximna.ordinal());
+                labels.add(metrica.getDescricao());
+                valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getRmTerra());
+                values.add(valorTemporario);
+            }
+
+            metrica = lstMetricas.get( AvaliacaoEnum.ForcaExplosiva.ordinal() );
+            labels.add(metrica.getDescricao());
+            valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getImpulsaoVertical());
+            values.add(valorTemporario);
+
+            metrica = lstMetricas.get( AvaliacaoEnum.ForcaIsometrica.ordinal() );
+            labels.add(metrica.getDescricao());
+            valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getForcaIsometricaMaos().toMinutes());
+            values.add(valorTemporario);
+
+            metrica = lstMetricas.get( AvaliacaoEnum.MobilidadeDoTornozeloDireito.ordinal() );
+            labels.add(metrica.getDescricao());
+            valorTemporario = getPercentualInvertido(metrica.getMinino(), metrica.getMaximo(), avaliacao.getTesteDeLungeJoelhoDireito());
+            values.add(valorTemporario);
+
+            metrica = lstMetricas.get( AvaliacaoEnum.MobilidadeDoTornozeloEsquerdo.ordinal() );
+            labels.add(metrica.getDescricao());
+            valorTemporario = getPercentualInvertido(metrica.getMinino(), metrica.getMaximo(), avaliacao.getTesteDeLungeJoelhoEsquerdo());
+            values.add(valorTemporario);
+
+            metrica = lstMetricas.get( AvaliacaoEnum.ResistenciaMuscularLocalizadaAbdominal.ordinal() );
+            labels.add(metrica.getDescricao());
+            valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getAbdominais());
+            values.add(valorTemporario);
+
+            metrica = lstMetricas.get( AvaliacaoEnum.ResistenciaMuscularLocalizadaMMSS.ordinal() );
+            labels.add(metrica.getDescricao());
+            valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getFlexoes());
+            values.add(valorTemporario);
+
+            metrica = lstMetricas.get( AvaliacaoEnum.ResistenciaAnaerobica.ordinal() );
+            labels.add(metrica.getDescricao());
+            valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getBurpees());
+            values.add(valorTemporario);
+
+            metrica = lstMetricas.get( AvaliacaoEnum.ResistenciaAerobica.ordinal() );
+            labels.add(metrica.getDescricao());
+            valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getCooper());
+            values.add(valorTemporario);
+
+            dto.labels = labels.toArray(new String[0]);
+            dto.values = values.stream().mapToDouble(Double::doubleValue).toArray();
+
+            return dto;
         }
-
-        metrica = lstMetricas.get(AvaliacaoEnum.ForcaExplosiva.ordinal());
-        labels.add(metrica.getDescricao());
-        valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getImpulsaoVertical());
-        values.add(valorTemporario);
-
-        metrica = lstMetricas.get(AvaliacaoEnum.ForcaIsometrica.ordinal());
-        labels.add(metrica.getDescricao());
-        valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getForcaIsometricaMaos().toMinutes());
-        values.add(valorTemporario);
-
-        metrica = lstMetricas.get(AvaliacaoEnum.MobilidadeDoTornozelo.ordinal());
-        labels.add(metrica.getDescricao());
-        valorTemporario = getPercentualInvertido(metrica.getMinino(), metrica.getMaximo(), avaliacao.getTesteDeLunge());
-        values.add(valorTemporario);
-
-        metrica = lstMetricas.get(AvaliacaoEnum.ResistenciaMuscularLocalizadaAbdominal.ordinal());
-        labels.add(metrica.getDescricao());
-        valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getAbdominais());
-        values.add(valorTemporario);
-
-        metrica = lstMetricas.get(AvaliacaoEnum.ResistenciaMuscularLocalizadaMMSS.ordinal());
-        labels.add(metrica.getDescricao());
-        valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getFlexoes());
-        values.add(valorTemporario);
-
-        metrica = lstMetricas.get(AvaliacaoEnum.ResistenciaAnaerobica.ordinal());
-        labels.add(metrica.getDescricao());
-        valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getBurpees());
-        values.add(valorTemporario);
-
-        metrica = lstMetricas.get(AvaliacaoEnum.ResistenciaAerobica.ordinal());
-        labels.add(metrica.getDescricao());
-        valorTemporario = getPercentual(metrica.getMinino(), metrica.getMaximo(), avaliacao.getCooper());
-        values.add(valorTemporario);
-
-        dto.labels = labels.toArray(new String[0]);
-        dto.values = values.stream().mapToDouble(Double::doubleValue).toArray();
-
-        return dto;
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 }
