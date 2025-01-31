4cf40db744af81da2bd522803503aa4cad383154
Michel Machado <michelmachado125@gmail.com>
2025-01-31 12:22:07 -0300

    [feat-] PISD-326 criar entidade grupo

---
+++ src/main/java/com/togather/sensei/models/classificacoes/GrupoModel.java
@@ -0,0 +1,23 @@
+package com.togather.sensei.models.classificacoes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "grupo_de_atletas_tb")
+public class GrupoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private boolean isAtivo;
+
+}
